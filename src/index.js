79d8e782829aab2d0e240a5f00fa9862935e1992
Jorge Bolaños <jorgedbolanosgh@gmail.com>
2024-11-06 19:23:33 -0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./../public/styles.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// const citrus = ["lime", "lemon", "orange"];
+// const fruits = ["apple", "banana", "coconut", ...citrus];
+
+// const fullName = {
+//   fName: "James",
+//   lName: "Bond",
+// };
+
+// const user = {
+//   ...fullName,
+//   username: "007",
+// };
+
+// console.log(user);
